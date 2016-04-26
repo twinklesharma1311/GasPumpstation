4ed62b801f2f508e71313ae15317a656cb427af8
rpgoncalves <rxp152830@utdallas.edu>
2016-04-26 04:30:03 -0500

    Removed src folder

--- src/main/java/com/pumpmachine/gaspumpstation/Diesel.java
+++
@@ -1,6 +0,0 @@
-package com.pumpmachine.gaspumpstation;
-/**
- * Created by twinklesharma on 4/23/16.
- */
-public class Diesel extends FuelType {
-}
--- src/main/java/com/pumpmachine/gaspumpstation/Grade.java
+++
@@ -1,5 +0,0 @@
-package com.pumpmachine.gaspumpstation;
-/**
- * Created by twinklesharma on 4/23/16.
- */
-public enum Grade { EIGHTYSEVEN, EIGHTYNINE, NINETYTHREE }
